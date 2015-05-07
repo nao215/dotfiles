4ff4743a2ab4c3562242d763075bc98da6152907
YanagisawaNao <Alexander@Alexander.local>
2015-05-07 11:24:54 +0900

    add .vim dir

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
+
+if [ -e ~/.vim ]; then
+  rm -rf ~/.vim
+fi
+
+if [ -e ~/.vimrc ]; then
+  rm ~/.vimrc
+fi
+
+if [ -e ~/.zshrc ]; then
+  rm ~/.zshrc
+fi
+
 ln -s ~/dotfiles/.vim   ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
